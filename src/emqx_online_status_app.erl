71513cdfcc54e0a02d3750a32e18c76140ded867
yqfclid <1192918988@qq.com>
2020-05-31 18:34:07 +0800

    update

--- src/emqx_online_status_app.erl
+++ src/emqx_online_status_app.erl
@@ -12,6 +12,7 @@
 start(_StartType, _StartArgs) ->
     emqx_online_status:load([]),
     start_redis_pool(),
+    io:format("emqx_online_status start ~n"),
     emqx_online_status_sup:start_link().
 
 stop(_State) ->
